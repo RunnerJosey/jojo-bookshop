fe5494173077957ecae0717c898cde25ee373001
RunnerJosey <oqiaoo@163.com>
2025-04-28 09:30:03 +0800

    修改启动提示

--- book-admin/src/main/java/com/book/BookshopAdminApplication.java
+++ book-admin/src/main/java/com/book/BookshopAdminApplication.java
@@ -15,6 +15,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class BookshopAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookshopAdminApplication.class,args);
-        log.info("===================本地开放开发平台启动成功==============");
+        log.info("=======各单位注意==============");
+        log.info("=======服务启动成功==============");
+        log.info("=======请慢用==============");
     }
 }
